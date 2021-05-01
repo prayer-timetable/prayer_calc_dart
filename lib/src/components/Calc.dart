1b92cf2cd843bf8e9754dfa01b24807751dfe926
ench0 <ensar@farend.net>
2021-05-01 18:19:37 +0100

    calc adjust

--- lib/src/components/Calc.dart
+++ lib/src/components/Calc.dart
@@ -18,9 +18,9 @@ class Calc {
 
   Calc(
     DateTime _time,
-    Prayers prayersToday,
-    Prayers prayersTomorrow,
-    Prayers prayersYesterday, {
+    Prayers _prayersToday,
+    Prayers _prayersTomorrow,
+    Prayers _prayersYesterday, {
     bool jamaahOn = false,
     Prayers? jamaahToday,
     Prayers? jamaahTomorrow,
@@ -28,169 +28,169 @@ class Calc {
     double lat = 0,
     double lng = 0,
   }) {
-    DateTime current = this.current;
-    DateTime next = this.next;
-    DateTime previous = this.previous;
-    int currentId = this.currentId;
-    bool isAfterIsha = false;
+    DateTime _current = this.current;
+    DateTime _next = this.next;
+    DateTime _previous = this.previous;
+    int _currentId = this.currentId;
+    bool _isAfterIsha = false;
 
     // time is local for PrayerTimetable and PrayerTimetableAlt
     // utc for PrayerTimetable
     /* *********************** */
-    /* CURRENT, PREVIOUS, NEXT */
+    /* _current, _previous, _next */
     /* *********************** */
     // from midnight to fajr
     if (!jamaahOn) {
-      if (_time.isBefore(prayersToday.dawn)) {
-        current = prayersYesterday.dusk;
-        next = prayersToday.dawn;
-        previous = prayersYesterday.sunset;
-        currentId = 5;
-      } else if (_time.isBefore(prayersToday.sunrise)) {
-        current = prayersToday.dawn;
-        next = prayersToday.sunrise;
-        previous = prayersYesterday.dusk;
-        currentId = 0;
-      } else if (_time.isBefore(prayersToday.midday)) {
-        current = prayersToday.sunrise;
-        next = prayersToday.midday;
-        previous = prayersToday.dawn;
-        currentId = 1;
-      } else if (_time.isBefore(prayersToday.afternoon)) {
-        current = prayersToday.midday;
-        next = prayersToday.afternoon;
-        previous = prayersToday.sunrise;
-        currentId = 2;
-      } else if (_time.isBefore(prayersToday.sunset)) {
-        current = prayersToday.afternoon;
-        next = prayersToday.sunset;
-        previous = prayersToday.midday;
-        currentId = 3;
-      } else if (_time.isBefore(prayersToday.dusk)) {
-        current = prayersToday.sunset;
-        next = prayersToday.dusk;
-        previous = prayersToday.afternoon;
-        currentId = 4;
+      if (_time.isBefore(_prayersToday.dawn)) {
+        _current = _prayersYesterday.dusk;
+        _next = _prayersToday.dawn;
+        _previous = _prayersYesterday.sunset;
+        _currentId = 5;
+      } else if (_time.isBefore(_prayersToday.sunrise)) {
+        _current = _prayersToday.dawn;
+        _next = _prayersToday.sunrise;
+        _previous = _prayersYesterday.dusk;
+        _currentId = 0;
+      } else if (_time.isBefore(_prayersToday.midday)) {
+        _current = _prayersToday.sunrise;
+        _next = _prayersToday.midday;
+        _previous = _prayersToday.dawn;
+        _currentId = 1;
+      } else if (_time.isBefore(_prayersToday.afternoon)) {
+        _current = _prayersToday.midday;
+        _next = _prayersToday.afternoon;
+        _previous = _prayersToday.sunrise;
+        _currentId = 2;
+      } else if (_time.isBefore(_prayersToday.sunset)) {
+        _current = _prayersToday.afternoon;
+        _next = _prayersToday.sunset;
+        _previous = _prayersToday.midday;
+        _currentId = 3;
+      } else if (_time.isBefore(_prayersToday.dusk)) {
+        _current = _prayersToday.sunset;
+        _next = _prayersToday.dusk;
+        _previous = _prayersToday.afternoon;
+        _currentId = 4;
       } else {
         // dusk till midnight
-        current = prayersToday.dusk;
-        next = prayersTomorrow.dawn;
-        previous = prayersToday.sunset;
-        currentId = 5;
-        isAfterIsha = true;
+        _current = _prayersToday.dusk;
+        _next = _prayersTomorrow.dawn;
+        _previous = _prayersToday.sunset;
+        _currentId = 5;
+        _isAfterIsha = true;
       }
     }
 
     // JAMAAH
-    bool jamaahPending = false;
+    bool _jamaahPending = false;
     if (jamaahOn) {
       // midnight - dawn
-      if (_time.isBefore(prayersToday.dawn)) {
-        current = prayersYesterday.dusk;
-        next = prayersToday.dawn;
-        previous = prayersYesterday.sunset;
-        currentId = 5;
+      if (_time.isBefore(_prayersToday.dawn)) {
+        _current = _prayersYesterday.dusk;
+        _next = _prayersToday.dawn;
+        _previous = _prayersYesterday.sunset;
+        _currentId = 5;
       }
       // dawn - fajr jamaah
       else if (_time.isBefore(jamaahToday!.dawn)) {
-        current = prayersToday.dawn;
-        next = jamaahToday.dawn;
-        previous = prayersToday.dawn;
-        currentId = 0;
-        jamaahPending = true;
+        _current = _prayersToday.dawn;
+        _next = jamaahToday.dawn;
+        _previous = _prayersToday.dawn;
+        _currentId = 0;
+        _jamaahPending = true;
       }
       // fajr jammah - sunrise
-      else if (_time.isBefore(prayersToday.sunrise)) {
-        current = prayersToday.dawn;
-        next = prayersToday.sunrise;
-        previous = prayersYesterday.dusk;
-        currentId = 0;
+      else if (_time.isBefore(_prayersToday.sunrise)) {
+        _current = _prayersToday.dawn;
+        _next = _prayersToday.sunrise;
+        _previous = _prayersYesterday.dusk;
+        _currentId = 0;
       }
       // sunrise - midday
-      else if (_time.isBefore(prayersToday.midday)) {
-        current = prayersToday.sunrise;
-        next = prayersToday.midday;
-        previous = prayersToday.dawn;
-        currentId = 1;
+      else if (_time.isBefore(_prayersToday.midday)) {
+        _current = _prayersToday.sunrise;
+        _next = _prayersToday.midday;
+        _previous = _prayersToday.dawn;
+        _currentId = 1;
       }
       // midday - dhuhr jamaah
       else if (_time.isBefore(jamaahToday.midday)) {
-        current = prayersToday.midday;
-        next = jamaahToday.midday;
-        previous = prayersToday.sunrise;
-        currentId = 2;
-        jamaahPending = true;
+        _current = _prayersToday.midday;
+        _next = jamaahToday.midday;
+        _previous = _prayersToday.sunrise;
+        _currentId = 2;
+        _jamaahPending = true;
       }
       // dhuhr jamaah - afternoon
-      else if (_time.isBefore(prayersToday.afternoon)) {
-        current = jamaahToday.midday;
-        next = prayersToday.afternoon;
-        previous = prayersToday.sunrise;
-        currentId = 2;
+      else if (_time.isBefore(_prayersToday.afternoon)) {
+        _current = jamaahToday.midday;
+        _next = _prayersToday.afternoon;
+        _previous = _prayersToday.sunrise;
+        _currentId = 2;
       }
       // afternoon - asr jamaah
       else if (_time.isBefore(jamaahToday.afternoon)) {
-        current = prayersToday.afternoon;
-        next = jamaahToday.afternoon;
-        previous = prayersToday.midday;
-        currentId = 3;
-        jamaahPending = true;
+        _current = _prayersToday.afternoon;
+        _next = jamaahToday.afternoon;
+        _previous = _prayersToday.midday;
+        _currentId = 3;
+        _jamaahPending = true;
       }
       // asr jamaah - sunset
-      else if (_time.isBefore(prayersToday.sunset)) {
-        current = prayersToday.afternoon;
-        next = prayersToday.sunset;
-        previous = prayersToday.midday;
-        currentId = 3;
+      else if (_time.isBefore(_prayersToday.sunset)) {
+        _current = _prayersToday.afternoon;
+        _next = _prayersToday.sunset;
+        _previous = _prayersToday.midday;
+        _currentId = 3;
       }
       // sunset - maghrib jamaah
       else if (_time.isBefore(jamaahToday.sunset)) {
-        current = prayersToday.sunset;
-        next = jamaahToday.sunset;
-        previous = prayersToday.afternoon;
-        currentId = 4;
-        jamaahPending = true;
+        _current = _prayersToday.sunset;
+        _next = jamaahToday.sunset;
+        _previous = _prayersToday.afternoon;
+        _currentId = 4;
+        _jamaahPending = true;
       }
       // maghrib jamaah - dusk
-      else if (_time.isBefore(prayersToday.dusk)) {
-        current = prayersToday.sunset;
-        next = prayersToday.dusk;
-        previous = prayersToday.afternoon;
-        currentId = 4;
+      else if (_time.isBefore(_prayersToday.dusk)) {
+        _current = _prayersToday.sunset;
+        _next = _prayersToday.dusk;
+        _previous = _prayersToday.afternoon;
+        _currentId = 4;
       }
       // dusk - isha jamaah
       else if (_time.isBefore(jamaahToday.dusk)) {
-        current = prayersToday.dusk;
-        next = jamaahToday.dusk;
-        previous = prayersToday.sunset;
-        currentId = 5;
-        jamaahPending = true;
+        _current = _prayersToday.dusk;
+        _next = jamaahToday.dusk;
+        _previous = _prayersToday.sunset;
+        _currentId = 5;
+        _jamaahPending = true;
       }
       // isha jamaah - midnight
       else {
-        current = prayersToday.dusk;
-        next = prayersTomorrow.dawn;
-        previous = prayersToday.sunset;
-        currentId = 5;
-        isAfterIsha = true;
+        _current = _prayersToday.dusk;
+        _next = _prayersTomorrow.dawn;
+        _previous = _prayersToday.sunset;
+        _currentId = 5;
+        _isAfterIsha = true;
       }
     }
 
     // components
     this.time = _time;
-    this.current = current;
-    this.next = next;
-    this.previous = previous;
-    this.isAfterIsha = isAfterIsha;
-    this.currentId = currentId;
-    this.countDown = next.difference(_time);
-    this.countUp = _time.difference(current);
+    this.current = _current;
+    this.next = _next;
+    this.previous = _previous;
+    this.isAfterIsha = _isAfterIsha;
+    this.currentId = _currentId;
+    this.countDown = _next.difference(_time);
+    this.countUp = _time.difference(_current);
 
     percentage = round2Decimals(100 *
         (this.countUp.inSeconds / (this.countDown + this.countUp).inSeconds));
 
     this.percentage = percentage.isNaN ? 0 : percentage;
-    this.jamaahPending = jamaahPending;
+    this.jamaahPending = _jamaahPending;
     this.qibla = Qibla.qibla(new Coordinates(lat, lng));
 
     //end
